6132911d9e08939838f8ccff0615b4c5e669cae7
Alejandrocsdev <alejandrocsdev@gmail.com>
2026-02-01 11:55:27 +0800

    add status, json to response object

--- response.js
+++ response.js
@@ -1,3 +1,24 @@
-const response = (res) => {};
+const { Buffer } = require('buffer');
+
+const response = (res) => {
+  res.status = (code) => {
+    res.statusCode = code;
+		// Returns the response object to enable chaining
+    return res;
+  };
+
+  res.json = (data) => {
+    const body = JSON.stringify(data);
+
+    if (!res.headersSent) {
+      res.setHeader('Content-Type', 'application/json');
+      res.setHeader('Content-Length', Buffer.byteLength(body));
+    }
+
+		// After write(), res.headersSent becomes true
+    res.write(body);
+    res.end();
+  };
+};
 
 module.exports = response;
